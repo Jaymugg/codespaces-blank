2c6490b6f4d08079fb909d4a9153d5f095bee720
Jmitchell11 <158069418+Jaymugg@users.noreply.github.com>
2024-02-21 15:35:23 +0000

    asynchworkfeb21

---
+++ feb21async.py
@@ -0,0 +1,14 @@
+Based on the video you just watched, in your own words, describe what is the Socratic Method
+In my own words the socratic method is basically 2 people going back and forth in a conversation 
+Why do you think it is important to asks questions in software engineering?
+Because If you don't know something then you can ask somebody so u can get help 
+Based on the video you just watched, in your own words, what do you think a data broker is?
+data broker is is like health insurance providers .
+Why do you think companies collect and sell people's data?
+to make money by selling it to the highest bidder .
+Based on the video you just watched, what does structural systemic racism mean? Provide 1 (one) example of structural system racism
+equality 
+Why do you think it is important to build algorithms where there exists racial context?
+important descisions 
+Why do you think it is important to have people of color involved in the development ofthese AI algorithms and systems?
+it's not ethical or biased 
