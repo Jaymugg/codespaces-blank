92816c0e4719edc338e9dd9155eab0bc0ff95dfd
Jmitchell11 <158069418+Jaymugg@users.noreply.github.com>
2024-03-06 16:16:27 +0000

    asyncreview

--- activitymar5.py
+++ activitymar5.py
@@ -1,3 +1,21 @@
 architecture 
 elevator 
 number 
+
+
+
+#keywords
+#parameter - data is passed inside of the function def.
+#function - our instrcutions need to be wrapped in a function . 
+#conditional statements - ( if / else / elif ) . 
+#comparison operators-check if age and height . 
+# match with our program requirements . 
+# interger datatype - age and float datatype for height 
+
+def rollercoaster ( height , age ):
+if height > = 5.2 
+print( you may get on rollercoaster #1)
+ elif height < 5.2 :
+ print(you may get on rollercoaster #2 )
+       else:
+       print(error : you may have typed something incorrectly.try again )      
